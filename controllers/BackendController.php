ddf51c6fa1646446861942cb6d674e811f49379e
Ivan Zavadsky <ivazin3@gmail.com>
2019-01-12 18:07:26 +0300

    Added file uploading functionality

--- controllers/BackendController.php
+++ controllers/BackendController.php
@@ -4,13 +4,32 @@ namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
+use app\models\UploadForm;
+use yii\web\UploadedFile;
 
 class BackendController extends Controller
 {
+    public $layout = 'admin_panel';
+
     public function actionIndex()
     {
-        $this->layout = 'admin_panel';
 
         return $this->render( 'index' );
     }
+
+    public function actionUpload()
+    {
+        $model = new UploadForm();
+
+        if (Yii::$app->request->isPost) {
+            $model->imageFile = UploadedFile::getInstance($model, 'imageFile');
+            if ($model->upload()) {
+                // file is uploaded successfully
+                return $this->render('upload', ['model' => $model]);
+            }
+        }
+
+        return $this->render('upload', ['model' => $model]);
+    }
+
 }
--- views/backend/index.php
+++ views/backend/index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo 66;
+echo "<a href='/index.php?r=backend%2Fupload'>upload file</a>";
